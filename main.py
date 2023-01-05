609d5d166be6378cba0bb15e119c66457f41f67f
vladpikaciu <70233497+vladpikaciu@users.noreply.github.com>
2023-01-05 16:39:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-from time import sleep
+﻿from time import sleep
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import random
-from selenium.webdriver.common.keys import Keys
 from datetime import datetime
-from termcolor import colored
-from selenium.webdriver.chrome.options import Options
+import termcolor
+from selenium.webdriver.firefox.options import Options
+import colorama
+from get_gecko_driver import GetGeckoDriver
+import csv
 
 
+colorama.init()
 # Curent Time
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 # Open file
-f = open("data.txt", "a")
-a = open("log.txt", "w+")
+with open ("Data.csv", "r") as data_file:
+    read = csv.reader(data_file)
+    rows = list(read)
+
 skin_stats = {
     'name': '',
     'selling_price': '',
@@ -26,13 +30,13 @@ skin_stats = {
     'buy_quantity': '',
 }
 order_quantity = 25
-# Create file
 
 
 def wait_till(element, seconds=10):
     WebDriverWait(driver, seconds).until(
         EC.presence_of_element_located((element))
     )
+
 
 def check_login():
     try:
@@ -42,30 +46,27 @@ def check_login():
         driver.get("https://steamcommunity.com/login/home/?goto=")
         username = input("Please enter steam username: ")
         password = input("Please enter steam password: ")
-        driver.find_element(by=By.ID, value="input_username").send_keys(username)
-        driver.find_element(by=By.ID, value="input_password").send_keys(password)
+        driver.find_element(By.CSS_SELECTOR, ".newlogindialog_TextField_2KXGK:nth-child(1) > .newlogindialog_TextInput_2eKVn").send_keys(username)
+        driver.find_element(By.CSS_SELECTOR, ".newlogindialog_TextField_2KXGK:nth-child(2) > .newlogindialog_TextInput_2eKVn").send_keys(password)
         try:
-            driver.find_element(by=By.ID, value="remember_login").click()
-            driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[7]/div[4]/div[1]/div/div[1]/div/div/div/div/div[3]/div[1]/button").click()
-            status = True
+            driver.find_element(By.CSS_SELECTOR, ".newlogindialog_SubmitButton_2QgFE").click()
+            if driver.find_element(By.CSS_SELECTOR, "input:nth-child(1)").is_displayed():
+                steam_guard = input("Please enter Steam Guard Code: ")
+                driver.find_element(By.CSS_SELECTOR, "input:nth-child(1)").send_keys(steam_guard)
+            else:
+                print("Couldn't find Steam Guard input. Continuing")
+            print("Logging IN....")
+            try:
+                wait_till((By.CLASS_NAME, "user_avatar"))
+                print("Logged in successfully.")
+                return True
+            except:
+                return False
         except:
-            driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[7]/div[4]/div[1]/div/div[1]/div/div/div/div/div[3]/div[1]/button").click()
-            status = False
-        try:
-            driver.find_element(by=By.ID, value="twofactorcode_entry")
-            steam_guard = input("Please enter Steam Guard Code: ")
-            driver.find_element(by=By.ID, value="twofactorcode_entry").send_keys(steam_guard)
-            driver.find_element(by=By.CSS_SELECTOR, value="#login_twofactorauth_buttonset_entercode > div.auth_button.leftbtn").click()
-            print("click")
-            sleep(2)
-            driver.get("https://steamcommunity.com/market/")
-            sleep(2)
-            driver.find_element(by=By.CLASS_NAME, value="avatarIcon")
-            sleep(2)
-        except:
-            print("Incorect login details")
-        return status
-# noinspection PyTypedDict
+            print("Submit button not visible.")
+            return False
+
+
 def scrape_page(min, max, orders, desired_profit):
     counter = 1
     for pages in range(min, max):
@@ -75,13 +76,15 @@ def scrape_page(min, max, orders, desired_profit):
             steam_url = 'https://steamcommunity.com/market/search?q=&category_730_ItemSet%5B0%5D=any&category_730_ProPlayer%5B0%5D=any&category_730_StickerCapsule%5B0%5D=any&category_730_TournamentTeam%5B0%5D=any&category_730_Weapon%5B0%5D=any&appid=730#p' + str(pages) + '_price_asc'
         elif game == 2:
             steam_url = 'https://steamcommunity.com/market/search?appid=304930#p' + str(pages) + '_quantity_desc'
-        driver.get(steam_url)
-        check_login()
-        print(check_login())
-        status = check_login()
-        if status == False:
+        elif game == 3:
+            steam_url = 'https://steamcommunity.com/market/search?appid=252490#p' + str(pages) + '_price_asc'
+        else:
+            steam_url = None
+            raise Exception("No game selected.")
+        status = False
+        while not status:
             driver.get(steam_url)
-            check_login()
+            status = check_login()
         driver.get(steam_url)
         sleep(1)
         # Scrape all skins links from steam search
@@ -181,39 +184,50 @@ def scrape_page(min, max, orders, desired_profit):
                     print('Page did not load correctly')
             finally:
                 profit = str(round(((round(skin_stats['selling_price'] / 1.15, 2) / skin_stats['buying_price']) - 1.02) * 100)) + '%'
-                a.write('\n' + str(round(skin_stats['selling_price'] / 1.15, 2)) + " | " + str(skin_stats['buying_price']) + ' | ' + profit + ' | ' + skin_stats['name'] + ' |  Page ' + str(pages))
                 # Check if it's profitable
                 if skin_stats['buy_quantity'] and skin_stats['sell_quantity'] >= orders:
                     if skin_stats['selling_price'] / 1.15 / skin_stats['buying_price'] > float(desired_profit):
                         profit = str(round(((round(skin_stats['selling_price'] / 1.15, 2) / skin_stats[
                             'buying_price']) - 1.02) * 100)) + '%'
-                        print(colored(str(round(skin_stats['selling_price'] / 1.15, 2)) + ' | ' + str(
+                        print("-"*50)
+                        print(termcolor.colored(str(round(skin_stats['selling_price'] / 1.15, 2)) + ' | ' + str(
                             skin_stats['buying_price']) + '  | ' + profit, 'magenta'))
-                        print(skin_stats['name'])
-                        f.write('\n' + str(round(skin_stats['selling_price'] / 1.15, 2)) + " | " + str(skin_stats['buying_price']) + ' | ' + profit + ' | ' + skin_stats['name'] + ' |  Page ' + str(pages))
-                print(colored(str(counter) + ' / ' + total_skins, 'yellow'))
-                print(colored('OK', 'green'))
+                        Item_name = driver.title
+                        Item_url = driver.current_url
+                        print (termcolor.colored(Item_name.replace("Steam Community Market :: Listings for ", ""), 'cyan'))
+                        print(termcolor.colored(Item_url, 'blue'))
+                        print("-"*50)
+
+                        new_sell = str(round(skin_stats['selling_price'] / 1.15, 2))
+                        new_buy = str(skin_stats['buying_price'])
+                        new_name = Item_name.replace("Steam Community Market :: Listings for ", "")
+                        new_name = new_name.replace("★", "")
+                        new_row = [new_sell, new_buy, profit, new_name, Item_url]
+                        with open("Data.csv", "a") as appendobj :
+                            append=csv.writer(appendobj)
+                            append.writerow(new_row)
+                print(termcolor.colored(str(counter) + ' / ' + total_skins, 'yellow'))
+                print(termcolor.colored('OK', 'green'))
                 counter += 1
                 if counter >= int(total_skins):
-                    print(colored(str(counter) + ' / ' + total_skins, 'yellow'))
-                    print(colored('Finished', 'grey'))
+                    print(termcolor.colored(str(counter) + ' / ' + total_skins, 'yellow'))
+                    print(termcolor.colored('Finished', 'grey'))
                     break
-                random_num = random.randint(15, 20)
-                # print('sleep ' + str(random_num))
-                sleep(random_num)
 
-headless = input("Type 1 to show chrome: ")
-chrome_options = Options()
+headless = input("Type 1 to show Firefox: ")
+firefox_options = Options()
 if headless != "1":
-    chrome_options.add_argument("--headless")
-chrome_options.add_argument("--user-data-dir=C:/data")
-chrome_options.add_extension('csgo_trader_extension.crx')
-s=Service("chromedriver.exe")
-driver = webdriver.Chrome(service=s, options=chrome_options)
+    firefox_options.add_argument("--headless")
+get_driver = GetGeckoDriver()
+get_driver.install()
+driver = webdriver.Firefox(options=firefox_options)
 
 
-print('Please select the game you want to scrape by typing the number 1 or 2')
-game = int(input('1.CSGO \n2.UNTURED  '))
+print('Please select the game you want to scrape by typing the number 1,2 or 3')
+game = int(input('1.CSGO\n2.UNTURED\n3.Rust\n'))
+while game not in [1,2,3]:
+    game=int(input('Please Type a valid number!\n1.CSGO\n2.UNTURED\n3.Rust\n'))
+
 print('The program will order the skins by price, from the cheapest to the most expensive skin.')
 start_page = int(input("Enter on which page to start: "))
 end_page = int(input("Enter on which page to end: "))
@@ -221,9 +235,6 @@ orders = int(input("Enter how many minimum orders should the item have: "))
 profit = float(input("Enter how much profit are you looking for (ex: 1.2): "))
 if game is not None:
     current_time = now.strftime("%H:%M:%S")
-    f.write('\n'+"Start Session     " + current_time + '\n')
     scrape_page(start_page, end_page, orders, profit)
-    f.write('\n')
     current_time = now.strftime("%H:%M:%S")
-    f.write("End Session     " + current_time + '\n')
     driver.quit()
